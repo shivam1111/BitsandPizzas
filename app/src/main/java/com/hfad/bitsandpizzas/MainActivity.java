69c47d0a9e3a67877374434a094ab5dfd56fe437
Shivam Goyal <shivam1111@gmail.com>
2021-12-20 21:18:09 +0530

    Page 451

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,45 +1,122 @@
 package com.hfad.bitsandpizzas;
 
-import android.support.v7.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.ShareActionProvider;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
     private String[] titles;
-    private ListView drawerList;
     private ShareActionProvider shareActionProvider;
+    // This will be placed programatically for adding toglle button
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public DrawerLayout drawerLayout;
+    public ListView drawerList;
+
+    //Implementation of DrawerLaout onClick Items
+    private class DrawerItemClickListener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    public void selectItem(int position){
+        Log.d("DrawerLayout","Item Clicked"+position);
+        // Code to run when item gets Clicked
+        Fragment fragment;
+        switch (position){
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame,fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+        ft.commit();
+        // Set the action bar title
+        setActionBarTitle(position);
+        drawerLayout.closeDrawer(drawerList);
+    }
+    private void setActionBarTitle(int position){
+        String title;
+        if (position == 0){
+            title = getResources().getString(R.string.app_name);
+        }else{
+            title = titles[position];
+        }
+        getSupportActionBar().setTitle(title);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get Titltes for DrawerLayout
         titles = getResources().getStringArray(R.array.titles);
-        drawerList = (ListView) findViewById(R.id.drawer);
+        // Get ListView of the Drawer Layout
+        drawerList = findViewById(R.id.drawer);
+        // Array Adapter to populate the ListView of Drawer Layout
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,titles);
+                android.R.layout.simple_list_item_activated_1,titles);
         drawerList.setAdapter(adapter);
+        // Attaching onItemClickListener to Drawer Layout
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        // Add Toggle Button to Drawer Layout in Action Bar
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                R.string.nav_open,R.string.nav_close);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
+
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
         setIntent("This is example text");
         return super.onCreateOptionsMenu(menu);
     }
 
+    // override the onOptionsItemSelected() function to implement
+    // the item click listener callback to open and close the navigation
+    // drawer when the icon is clicked
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch(item.getItemId()){
             case R.id.action_settings:
                 return true;
